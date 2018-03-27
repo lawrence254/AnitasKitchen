0dfe7e69b12e79f3d00949e02d2f6ba8ea524e5e
Lawrence <lawrenslava1@gmail.com>
2018-03-27 10:14:05 +0300

    Redirect submit form upon click to mailchimp

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
     $('.fullscreen-nav').toggleClass('open');
 	});
+  $('.subButton').submit(function (event) {
+    event.preventDefault();
+    var emailInput=$("input#email").val();
+    alert(emailInput+" has been successfully added to our email list. Thank you.")
+  });
 });
